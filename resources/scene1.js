ccac1f08ccfeb2ee4d5974f7bd9520e2807fac8d
saxenaii <saxenaii@mail.uc.edu>
2023-03-21 19:05:23 -0400

    Adding assets and first scene

---
+++ resources/scene1.js
@@ -0,0 +1,27 @@
+class scene1 extends Phaser.Scene {
+  constructor() {
+    // Name this scene 
+    super("bootGame");
+  }
+
+  preload() {
+    this.load.image("background", "assets/images/background.png");
+    //this.load.image("ship", "assets/images/ship.png");
+    //this.load.image("ship2", "assets/images/ship2.png");
+    //this.load.image("ship3", "assets/images/ship3.png");
+
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png", { frameWidth: 16, frameHeight: 16 });
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", { frameWidth: 32, frameHeight: 16 });
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", { frameWidth: 16, frameHeight: 16 });
+    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png", { frameWidth: 16, frameHeight: 16 });
+    this.load.spritesheet("player", "assets/spritesheets/player.png", { frameWidth: 16, frameHeight: 24 });
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png", { frameWidth: 16, frameHeight: 16 });
+    this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml")
+  }
+
+  create() {
+
+  }
+
+}
